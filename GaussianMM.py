9ca9269531b63362fc9cb96a1e63ecca97a1f49a
jhopkins39 <jhopkins39@gatech.edu>
2022-04-05 19:53:31 -0400

    just what will run for now for GMM - couldn't get it to work so pivoting now

--- GaussianMM.py
+++ GaussianMM.py
@@ -1,3 +1,6 @@
+# SCRATCHED FOR NOW
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -18,7 +21,7 @@ def process_data():
 def use_model():
 
     ds = dataset.Dataset()
-    curr_data = ds.getRegularGames(["Wscore","Lscore", "Numot"])[1]
+    curr_data = ds.getRegularGames(headers=["Wscore","Lscore", "Numot"],season=2016)[1]
     curr_data = np.array(curr_data)
 
     X_train = curr_data
@@ -26,27 +29,39 @@ def use_model():
     print("Xtrain length: ")
     print(len(X_train))
     print("Xtrain: ")
-    print(X_train)
+    #print(X_train)
 
 
     # fit a Gaussian Mixture Model with two components
     clf = mixture.GaussianMixture(n_components=2, covariance_type="full")
     clf.fit(X_train)
 
-    # display predicted scores by the model as a contour plot
-    x = np.linspace(-20.0, 30.0)
-    y = np.linspace(-20.0, 40.0)
-    X, Y = np.meshgrid(x, y)
-    XX = np.array([X.ravel(), Y.ravel()]).T
-    Z = -clf.score_samples(XX)
-    Z = Z.reshape(X.shape)
 
-    CS = plt.contour(
-        X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0), levels=np.logspace(0, 3, 10)
-    )
-    CB = plt.colorbar(CS, shrink=0.8, extend="both")
-    plt.scatter(X_train[:, 0], X_train[:, 1], 0.8)
+    newX,newY = clf.sample(100)
 
-    plt.title("Negative log-likelihood predicted by a GMM")
-    plt.axis("tight")
+    print(newX)
+    print(newY)
+
+    plt.scatter(newX,newY)
+    #plt.scatter(X_train[:,0], X_train[:,1])
     plt.show()
+
+
+
+    # display predicted scores by the model as a contour plot
+    # x = np.linspace(-20.0, 30.0)
+    # y = np.linspace(-20.0, 40.0)
+    # X, Y = np.meshgrid(x, y)
+    # XX = np.array([X.ravel(), Y.ravel()]).T
+    # Z = -clf.score_samples(XX)
+    # Z = Z.reshape(X.shape)
+
+    # CS = plt.contour(
+    #     X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0), levels=np.logspace(0, 3, 10)
+    # )
+    # CB = plt.colorbar(CS, shrink=0.8, extend="both")
+    # plt.scatter(X_train[:, 0], X_train[:, 1], 0.8)
+
+    # plt.title("Negative log-likelihood predicted by a GMM")
+    # plt.axis("tight")
+    # plt.show()
