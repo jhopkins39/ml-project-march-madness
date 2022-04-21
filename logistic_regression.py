2dafc9a3f29d5ceb89711c830cfc032f57af2e1b
jhopkins39 <jhopkins39@gatech.edu>
2022-04-21 02:25:58 -0400

    Finished the logistic regression model.  It isn't awesome, but I would say it works much awesomer.  Somethings might need to be fine tuned for the final project and to get statistics for a graph or something like that

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,43 +6,49 @@ from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import r2_score
 import csv
 import dataset
+import sys
+from sklearn.decomposition import PCA
 
-headernNames = [
-	'Wscore',
-	'Lscore',
-	'Wloc',  
-	'Numot',
+# the header names of the original dataframe from dataset class
+def get_header_names():
+	headerNames = [
+		'Wscore',
+		'Lscore',
+		'Wloc',  
+		'Numot',
 
-	'Wfgm',  
-	'Wfga',
-	'Wfgm3',  
-	'Wfga3',  
-	'Wftm',  
-	'Wfta',   
-	'Wor',   
-	'Wdr',  
-	'Wast',   
-	'Wto',  
-	'Wstl',  
-	'Wblk',   
-	'Wpf',  
+		'Wfgm',  
+		'Wfga',
+		'Wfgm3',  
+		'Wfga3',  
+		'Wftm',  
+		'Wfta',   
+		'Wor',   
+		'Wdr',  
+		'Wast',   
+		'Wto',  
+		'Wstl',  
+		'Wblk',   
+		'Wpf',  
 
-	'Lfgm',  
-	'Lfga',  
-	'Lfgm3',  
-	'Lfga3',  
-	'Lftm',  
-	'Lfta',   
-	'Lor',   
-	'Ldr',  
-	'Last',   
-	'Lto',  
-	'Lstl',  
-	'Lblk',   
-	'Lpf'
-	]
+		'Lfgm',  
+		'Lfga',  
+		'Lfgm3',  
+		'Lfga3',  
+		'Lftm',  
+		'Lfta',   
+		'Lor',   
+		'Ldr',  
+		'Last',   
+		'Lto',  
+		'Lstl',  
+		'Lblk',   
+		'Lpf'
+		]
+	return headerNames
 
 
+# dictionary of all the final four teams for each year - easily indexed
 def get_final4teams():
 	final4teams = {
 		2003: (1242,1266,1393,1400),
@@ -63,24 +69,18 @@ def get_final4teams():
 	return final4teams
 
 
-# right now it adds all the final four values for the teams, paired with their seeds only
+# 4774 data points - only 56 with a label of 1
+# returns a numpy array where the last onecolumn is the label and the other 30 something columns are the data point averages
+# should average correctly and returns exactly what we want to use these algorithms
 def process_data():
 
 	ds = dataset.Dataset()
 	years = ds.getYears(compact=False)
 	years.remove(2017)
-
 	final4teams = get_final4teams()
 
-
-	# game_teams, game_stats = ds.getRegularGames(season = years, compact=False)
-	# game_teams = np.array(game_teams)
-	# game_stats = np.array(game_stats)
-	# print(game_teams)
-	# print(game_stats)
-
-
 	final_stats = np.zeros(())
+	counter = 0
 
 	for year in years:
 		game_teams, game_stats = ds.getRegularGames(season = year, compact=False)
@@ -100,61 +100,131 @@ def process_data():
 			w_score = np.mean(info_to_average_w, axis=0)[0]
 			l_score = np.mean(info_to_average_l, axis=0)[1]
 			label = 1 if team in final4teams[year] else 0
-			print(w_score)
-			print(l_score)
-			print(w_mean)
-			print(l_mean)
-			curr_final = [num_w, num_l, w_score, l_score, *list(w_mean), *list(l_mean)]
-			print()
-			print(curr_final)
-			return curr_final
+			curr_final = [num_w, num_l, w_score, l_score, *list(w_mean), *list(l_mean), label]
+			if counter == 0:
+				final_stats = np.array(curr_final)
+				counter += 1
+			else:
+				final_stats = np.vstack((final_stats,curr_final))
+
+	return final_stats
+
+# gets your labels and your test / train data set
+# the test_train number is exactly one season's worth of data - essentially we'll be guessing 2016 based on 2003-2015 data
+def separate_data(data, test_train=(1/14)):
+
+	N = len(data)
+
+	train = data[0:int(N*(1-test_train))]
+	test = data[int(N*(1-test_train)):]
+
+	d = len(train[0])
+
+	train_labels = train[:,d-1]
+	test_labels = test[:,d-1]
+
+	train = train[:,0:d-2]
+	test = test[:,0:d-2]
+
+	train[np.isnan(train)] = np.median(train[~np.isnan(train)])
+	train_labels[np.isnan(train_labels)] = np.median(train_labels[~np.isnan(train_labels)])
+	test[np.isnan(test)] = np.median(test[~np.isnan(test)])
+	test_labels[np.isnan(test_labels)] = np.median(test_labels[~np.isnan(test_labels)])
+
+	return train, train_labels, test, test_labels
+
+# runs principle componenet analysis on my trainging and testing data :)
+def apply_PCA(train_x, test_x, num_components):
+	# at the current state of the data 4/10/22 4 is the optimal number of components
+	pca = PCA(num_components)
+	pca.fit(train_x)
+
+	# print("explained_variance_ratio: ")
+	# print(pca.explained_variance_ratio_)
+	# print("singular_values: ")
+	# print(pca.singular_values_)
+	# print()
+	# print()
+
+	new_train = pca.transform(train_x)
+	new_test = pca.transform(test_x)
+	return new_train, new_test
 
 
-	
+def use_model(num_components=None):
 
+	data = process_data()
 
+	# np.set_printoptions(threshold=sys.maxsize)
+	# print(data)
 
+	X, y, test_X, test_y = separate_data(data)
+	if num_components != None:
+		X, test_X = apply_PCA(X, test_X, num_components)
 
+	model = LogisticRegression(max_iter=100000)
+	model.fit(X, y)
 
+	predictions = model.predict(test_X)
+	print("model predictions: ")
+	print(predictions)
 
+	print("actual: ")
+	print(test_y)
 
+	probabilities = model.predict_proba(test_X)
+	print("probabilities: ")
+	print(probabilities)
 
+	score = model.score(test_X, test_y)
+	print("score: ")
+	print(score)
 
-	teamIDs = list()
-	stats = list()
+	evs = explained_variance_score(predictions, test_y)
+	print("explained_variance_score: ")
+	print(evs)
 
-	final = list()
-	for year in years:
-		seeds = ds.getSeeds(season=year)
-		
-		team = list(seeds.keys())
-		seed = np.array(list(seeds.values()))
-		
-		final4 = final4teams[year]
-		curr_indices = list()
-		for ffid in final4:
-			curr_indices.append(team.index(ffid))
+	mape = mean_absolute_percentage_error(predictions, test_y)
+	print("mean_absolute_percentage_error: ")
+	print(mape)
 
-		labels = np.zeros((len(seed)))
+	r2 = r2_score(predictions, test_y)
+	print("r2_score: ")
+	print(r2)
 
-		for index in curr_indices:
-			labels[index] = 1
+	return probabilities, test_y
 
-		curr_final = np.vstack((seed,labels))
-		curr_final = curr_final.T
+# two hyperparameters to "tune" for our project
+# based on playing around with it, PCA does not help and top 4 or top 8 probabilities doesn't matter
+def analyze_results(num_components=None, num_max=8):
 
-		for i in range(len(curr_final)):
-			final.append(curr_final[i])
+	probabilities, actual_y = use_model(num_components)
 
-	final = np.array(final)
-	return final
+	maximum_index = np.argpartition(probabilities[:,1], -num_max)[-num_max:]
+	max_probabilities = probabilities[maximum_index]
+	actual_from_probabilities = actual_y[maximum_index]
+	better_score = np.sum(actual_from_probabilities) / len(actual_from_probabilities)
 
-print(len(process_data()))
+	# index_predict = np.argpartition(actual_y, -4)[-4:]
+	# answers = actual_y[index_predict]
 
+	print()
+	print("best probabilities:  ")
+	print(max_probabilities)
 
+	print()
+	print("best probabilities indexes:  ")
+	print(maximum_index)
 
+	print()
+	print("actual values of highest probability likelihood from logistic regression model: ")
+	print(actual_from_probabilities)
 
+	print()
+	print("a better score: ")
+	print(better_score)
 
+analyze_results()
 
 
 
